62786b3dc33dff17d7c1788713a77e3bbb3739de
slash0593 <absicamara@gmail.com>
2018-01-02 21:31:44 +0000

    before tuto section editing

---
+++ VUE/INCLUDES/newnav.php
@@ -0,0 +1,77 @@
+<nav class="navbar navbar-toggleable-md navbar-inverse ">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        <div></div></button>
+    <a class="navbar-brand" href="#">Navbar</a>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+            <input class="form-control mr-sm-2" placeholder="Search" type="text">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+
+
+
+<footer>
+    <div class="row" id="foot">
+        <div class="col-lg-4">
+            <p class="footer_title"> Check Out</p>
+            <ul class="footer_link_ressources">
+
+                <li> <a class="footer_link" href="mailto:absi.camara@outlook.fr"><span class="glyphicon glyphicon-"></span> Services</a></li>
+                <li> <a class="footer_link" href="mailto:absi.camara@outlook.fr"><span class="glyphicon glyphicon-"></span> Geeksh Tutorials</a></li>
+                <li> <a class="footer_link" href="mailto:absi.camara@outlook.fr"><span class="glyphicon glyphicon-gears"></span> Geeksh campaigns</a></li>
+
+            </ul>
+        </div>
+        <div class="col-lg-4">
+            <p class="footer_title"> Reach Out</p>
+            <ul class="footer_link_contacts">
+                <li> <a class="footer_link" href="mailto:absi.camara@outlook.fr"><span class="glyphicon glyphicon-phone"></span> +221 77 571 76 82</a></li>
+                <li> <a class="footer_link" href="mailto:absi.camara@outlook.fr"><span class="glyphicon glyphicon-phone"></span> +221 77 710 05 29</a></li>
+                <li> <a class="footer_link" href="mailto:absi.camara@outlook.fr"><span class="glyphicon glyphicon-envelope">  	</span>  Envoyer un mail</a></li>
+
+            </ul>
+        </div>
+        <div class="col-lg-4">
+            <p id="footer_geeksh"> GEEKSH </p>
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+
+<h4>Contact info</h4>
+<div class="space"></div>
+<p>
+    <i class="fa fa-map-marker"></i>4321 California St,<br>
+    San Francisco, CA 12345
+</p>
+<div class="space"></div>
+<p><i class="fa fa-envelope-o"></i>contact@ateimagine.com</p>
+<div class="space"></div>
+<p><i class="fa fa-phone"></i>+1 123 456 1234</p>
